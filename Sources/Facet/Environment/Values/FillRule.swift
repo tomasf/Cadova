c0184be711d4fdb488963497f2f8c8281208e2cb
Tomas Franzén <tomas@tomasf.se>
2024-12-24 19:51:46 +0100

    Add FillRule

---
+++ Sources/Facet/Environment/Values/FillRule.swift
@@ -0,0 +1,44 @@
+import Foundation
+import Manifold
+
+public extension EnvironmentValues {
+    static private let environmentKey = Key("Facet.FillRule")
+
+    var fillRule: FillRule {
+        self[Self.environmentKey] as? FillRule ?? .default
+    }
+
+    func withFillRule(_ fillRule: FillRule) -> EnvironmentValues {
+        setting(key: Self.environmentKey, value: fillRule)
+    }
+}
+
+public enum FillRule: Hashable, Sendable {
+    case nonZero
+    case evenOdd
+    case positive
+    case negative
+
+    static var `default`: FillRule { .nonZero }
+
+    internal var primitive: CrossSection.FillRule {
+        switch self {
+        case .nonZero: return .nonZero
+        case .evenOdd: return .evenOdd
+        case .positive: return .positive
+        case .negative: return .negative
+        }
+    }
+}
+
+public extension Geometry2D {
+    func usingFillRule(_ fillRule: FillRule) -> Geometry2D {
+        withEnvironment { $0.withFillRule(fillRule) }
+    }
+}
+
+public extension Geometry3D {
+    func usingFillRule(_ fillRule: FillRule) -> Geometry3D {
+        withEnvironment { $0.withFillRule(fillRule) }
+    }
+}
--- Sources/Facet/Values/Manifold.swift
+++ Sources/Facet/Values/Manifold.swift
@@ -26,15 +26,19 @@ public protocol PrimitiveGeometry {
     var boundingBox: (Vector, Vector) { get }
     var vertexCount: Int { get }
 
-    func boolean(_ op: BooleanOperation, with other: Self) -> Self
-    static func boolean(_ op: BooleanOperation, with children: [Self]) -> Self
-
     func transform(_ transform: Matrix) -> Self
     func translate(_ translation: Vector) -> Self
     func scale(_ scale: Vector) -> Self
     func rotate(_ rotation: Rotation) -> Self
 
+    func boolean(_ op: BooleanOperation, with other: Self) -> Self
+    static func boolean(_ op: BooleanOperation, with children: [Self]) -> Self
+
     func hull() -> Self
+    static func hull(_ children: [Self]) -> Self
+    static func hull(_ points: [Vector]) -> Self
+
+    func warp(_ function: @escaping (Vector) -> Vector) -> Self
 }
 
 extension CrossSection: PrimitiveGeometry {
