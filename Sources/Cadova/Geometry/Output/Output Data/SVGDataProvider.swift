1192685cdaa6d7c9053c65e86d803072d24bf504
Tomas Franzén <tomas@tomasf.se>
2025-04-16 22:45:06 +0200

    Use Nodal for SVGDataProvider

--- Sources/Cadova/Geometry/Output/Output Data/SVGDataProvider.swift
+++ Sources/Cadova/Geometry/Output/Output Data/SVGDataProvider.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Nodal
 
 struct SVGDataProvider: OutputDataProvider {
     let output: Output2D
@@ -9,43 +10,20 @@ struct SVGDataProvider: OutputDataProvider {
         let bounds = primitive.bounds
         let shapePoints = primitive.polygons()
 
-        let svg = XMLElement(name: "svg")
-        svg["xmlns"] = "http://www.w3.org/2000/svg"
-        svg["viewBox"] = String(format: "%g %g %g %g",
-                                bounds.min.x, bounds.min.y,
-                                bounds.max.x - bounds.min.x, bounds.max.y - bounds.min.y)
+        let document = Document()
+        let svg = document.makeDocumentElement(name: "svg", defaultNamespace: "http://www.w3.org/2000/svg")
+        svg[attribute: "viewBox"] = String(format: "%g %g %g %g",
+                                           bounds.min.x, bounds.min.y,
+                                           bounds.max.x - bounds.min.x, bounds.max.y - bounds.min.y)
 
-        let path = XMLElement(name: "path")
-        path["fill"] = "black"
-        path["d"] = shapePoints.map {
+        let path = svg.addElement("path")
+        path[attribute: "fill"] = "black"
+        path[attribute: "d"] = shapePoints.map {
             "M " + $0.vertices.map {
                 String(format: "%g,%g", $0.x, $0.y)
             }.joined(separator: " ")
         }.joined(separator: " ")
 
-        svg.addChild(path)
-
-        let document = XMLDocument(rootElement: svg)
-        return document.xmlData(options: .nodeCompactEmptyElement)
-    }
-}
-
-
-fileprivate extension XMLElement {
-    subscript(attribute: String) -> String? {
-        get {
-            self.attribute(forName: attribute)?.stringValue
-        }
-        set {
-            if let newValue {
-                if let existingAttribute = self.attribute(forName: attribute) {
-                    existingAttribute.stringValue = newValue
-                } else {
-                    addAttribute(XMLNode.attribute(withName: attribute, stringValue: newValue) as! XMLNode)
-                }
-            } else {
-                self.removeAttribute(forName: attribute)
-            }
-        }
+        return try document.xmlData()
     }
 }
