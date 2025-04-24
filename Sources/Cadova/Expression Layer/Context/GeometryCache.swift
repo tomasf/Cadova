b289ea66ea24302181ff084126460c5cc8690272
Tomas Franzén <tomas@tomasf.se>
2025-04-24 15:32:02 +0200

    Make GeometryCache generic

--- Sources/Cadova/Expression Layer/Context/GeometryCache.swift
+++ Sources/Cadova/Expression Layer/Context/GeometryCache.swift
@@ -1,45 +1,30 @@
 import Foundation
 import Manifold3D
 
-actor GeometryCache {
-    private var cache2D: [GeometryExpression2D: Task<CrossSection, Never>] = [:]
-    private var cache3D: [GeometryExpression3D: Task<Manifold, Never>] = [:]
+actor GeometryCache<D: Dimensionality> {
+    private var entries: [D.Expression: Task<D.Primitive, Never>] = [:]
 
-    func cachedGeometry(for expression: GeometryExpression2D) async -> CrossSection? {
-        await cache2D[expression]?.value
+    func cachedGeometry(for expression: D.Expression) async -> D.Primitive? {
+        await entries[expression]?.value
     }
 
-    func cachedGeometry(for expression: GeometryExpression3D) async -> Manifold? {
-        await cache3D[expression]?.value
-    }
-
-    func geometry(for expression: GeometryExpression2D, in context: EvaluationContext) async -> CrossSection {
+    func geometry(for expression: D.Expression, in context: EvaluationContext) async -> D.Primitive {
         guard !expression.isEmpty else { return .empty }
 
         if let cached = await cachedGeometry(for: expression) {
             return cached
         }
         let task = Task { await expression.evaluate(in: context) }
-        cache2D[expression] = task
+        entries[expression] = task
         return await task.value
     }
 
-    func geometry(for expression: GeometryExpression3D, in context: EvaluationContext) async -> Manifold {
-        guard !expression.isEmpty else { return .empty }
-
-        if let cached = await cachedGeometry(for: expression) {
-            return cached
-        }
-        let task = Task { await expression.evaluate(in: context) }
-        cache3D[expression] = task
-        return await task.value
+    var count: Int {
+        entries.count
     }
 
-    // For testing
-    internal var cachedCount2D: Int { cache2D.count }
-    internal var cachedCount3D: Int { cache3D.count }
-    internal func printExpressions() {
-        for key in cache3D.keys {
+    func debugPrint() {
+        for key in entries.keys {
             print(key.debugDescription)
         }
     }
--- Tests/Tests/Cache.swift
+++ Tests/Tests/Cache.swift
@@ -9,23 +9,23 @@ struct GeometryCacheTests {
 
     @Test func basics() async throws {
         _ = await context.primitive(for: sphere)
-        await #expect(context.cache.cachedCount3D == 1)
+        await #expect(context.cache3D.count == 1)
 
         let union = sphere.adding { box }
 
         _ = await context.primitive(for: union)
-        await #expect(context.cache.cachedCount3D == 3)
+        await #expect(context.cache3D.count == 3)
 
         _ = await context.primitive(for: union)
-        await #expect(context.cache.cachedCount3D == 3)
+        await #expect(context.cache3D.count == 3)
     }
 
     @Test func differentEnvironments() async throws {
         _ = await context.primitive(for: sphere)
-        await #expect(context.cache.cachedCount3D == 1)
+        await #expect(context.cache3D.count == 1)
 
         _ = await context.primitive(for: sphere, in: .defaultEnvironment.withSegmentation(.fixed(10)))
-        await #expect(context.cache.cachedCount3D == 2)
+        await #expect(context.cache3D.count == 2)
     }
 
     @Test func warp() async throws {
@@ -35,27 +35,27 @@ struct GeometryCacheTests {
         }
 
         _ = await context.primitive(for: warp1)
-        await #expect(context.cache.cachedCount3D == 2) // sphere + warped box
+        await #expect(context.cache3D.count == 2) // sphere + warped box
 
         _ = await context.primitive(for: warp1)
-        await #expect(context.cache.cachedCount3D == 2)
+        await #expect(context.cache3D.count == 2)
 
         let warp2 = box.warped(operationName: "test", cacheParameters: scale) { _ in
             Issue.record("This closure should never be reached because the cache keys are matching")
             return .zero
         }
         _ = await context.primitive(for: warp2)
-        await #expect(context.cache.cachedCount3D == 2)
+        await #expect(context.cache3D.count == 2)
     }
 
     @Test func split() async throws {
-        await #expect(context.cache.cachedCount3D == 0)
+        await #expect(context.cache3D.count == 0)
 
         let split1 = box.split(along: .init(z: 2).rotated(x: 10°)) { g1, g2 in
             g1.adding(g2)
         }
 
         _ = await context.primitive(for: split1)
-        await #expect(context.cache.cachedCount3D == 6) // box, 2x splits, 2x translated splits, split union
+        await #expect(context.cache3D.count == 6) // box, 2x splits, 2x translated splits, split union
     }
 }
