f47e6159eeaa2636f4ebfdb82225678098e68698
Tomas Franzén <tomas@tomasf.se>
2025-04-26 00:25:01 +0200

    Improve cached docs

--- Sources/Cadova/Operations/Cached.swift
+++ Sources/Cadova/Operations/Cached.swift
@@ -1,6 +1,21 @@
 import Foundation
 
 extension Geometry {
+    /// Caches this geometry under a specified name and optional parameters.
+    ///
+    /// Use this method to optimize complex or expensive geometry by wrapping it in a caching layer.
+    /// Caching helps avoid redundant computation of both the geometry expression and the underlying primitive data.
+    ///
+    /// The operation is cached based on the supplied `name` and `parameters`. If the same
+    /// combination of input geometry and cache parameters has been previously evaluated, the cached result is reused
+    /// to avoid redundant computation. Ensure that these parameters are stable and deterministic;
+    /// the same set of name + parameters should always result in an identical operation.
+    ///
+    /// - Parameters:
+    ///   - name: A descriptive name identifying the cached geometry operation.
+    ///   - cacheKeys: Additional values that influence caching behavior.
+    /// - Returns: A new, cached version of this geometry.
+    ///
     func cached(as name: String, parameters cacheKeys: any CacheKey...) -> D.Geometry {
         CachedBoxedGeometry(key: NamedCacheKey(operationName: name, parameters: cacheKeys)) {
             self
