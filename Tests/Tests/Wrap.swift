f62f30bd90cdcef2ab61b535ff678634de98548c
Tomas Franzén <tomas@tomasf.se>
2025-04-26 17:36:58 +0200

    Add wrappedAroundSphere

---
+++ Tests/Tests/Wrap.swift
@@ -0,0 +1,28 @@
+import Testing
+@testable import Cadova
+
+struct WrapTests {
+    @Test func wrapAroundSphere() async throws {
+        try await Box([40, 30, 3])
+            .aligned(at: .centerXY)
+            .adding {
+                Sphere(diameter: 3)
+                    .translated(z: 3)
+                    .repeated(along: .x, step: 5, count: 7)
+                    .repeated(along: .y, step: 5, count: 5)
+                    .aligned(at: .centerXY)
+            }
+            .aligned(at: .left)
+            .adding {
+                Cylinder(diameter: 2, height: 30)
+                    .rotated(x: -90°)
+                    .aligned(at: .centerY)
+                    .translated(z: 3)
+                    .within(x: 0...)
+                    .colored(.red)
+            }
+            .translated(x: -5)
+            .wrappedAroundSphere(diameter: 30)
+            .expectEquals(goldenFile: "wrapAroundSphere")
+    }
+}
