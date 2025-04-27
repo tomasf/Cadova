0577df192cf3bb34b57e33377b804004a3b27e81
Tomas Franzén <tomas@tomasf.se>
2025-04-27 20:06:59 +0200

    Enable lazy union only for stack spacing >= 0

--- Sources/Cadova/Operations/Boolean/LazyUnion.swift
+++ Sources/Cadova/Operations/Boolean/LazyUnion.swift
@@ -1,18 +1,18 @@
 import Foundation
 import Manifold3D
 
-internal func LazyUnion<D: Dimensionality>(children: [D.Geometry]) -> D.Geometry {
-    if let children = children as? [any Geometry3D] {
+internal func LazyUnion<D: Dimensionality>(lazy: Bool, children: [D.Geometry]) -> D.Geometry {
+    if lazy, let children = children as? [any Geometry3D] {
         GeometryExpressionTransformer<D3, D3>(bodies: children) { .lazyUnion($0) } as! D.Geometry
     } else {
         Union(children)
     }
 }
 
-internal func LazyUnion<D: Dimensionality>(@ArrayBuilder<D.Geometry> _ body: () -> [D.Geometry]) -> D.Geometry {
-    LazyUnion(children: body())
+internal func LazyUnion<D: Dimensionality>(lazy: Bool, @ArrayBuilder<D.Geometry> _ body: () -> [D.Geometry]) -> D.Geometry {
+    LazyUnion(lazy: lazy, children: body())
 }
 
-internal func LazyUnion<D: Dimensionality>(@ArrayBuilder<D.Geometry> _ body: () async -> [D.Geometry]) async -> D.Geometry {
-    await LazyUnion(children: body())
+internal func LazyUnion<D: Dimensionality>(lazy: Bool, @ArrayBuilder<D.Geometry> _ body: () async -> [D.Geometry]) async -> D.Geometry {
+    await LazyUnion(lazy: lazy, children: body())
 }
