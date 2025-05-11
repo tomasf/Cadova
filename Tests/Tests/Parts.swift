529f0dff46bb49b86414b52bac65ac02d60741b7
Tomas Franzén <tomas@tomasf.se>
2025-05-11 13:41:48 +0200

    Add more part tests

--- Tests/Tests/Parts.swift
+++ Tests/Tests/Parts.swift
@@ -11,4 +11,36 @@ struct PartTests {
             .translated(y: 10)
             .expectEquals(goldenFile: "separatePart")
     }
+
+    @Test func nestedParts() async throws {
+        let g = Box(10)
+            .adding {
+                Sphere(diameter: 10)
+                    .inPart(named: "inner")
+                    .adding {
+                        Sphere(diameter: 5)
+                    }
+                    .inPart(named: "outer")
+            }
+            .translated(x: 10)
+
+        let partNames = await g.parts.map(\.key.name)
+        #expect(partNames == ["inner", "outer"])
+    }
+
+    @Test func mergedParts() async throws {
+        let g = Box(10)
+            .adding {
+                Sphere(diameter: 5)
+                    .inPart(named: "merged")
+            }
+            .subtracting {
+                Box(x: 20, y: 4, z: 4)
+                    .inPart(named: "merged")
+            }
+
+        let node = try await #require(g.parts[.named("merged", type: .solid)]?.node)
+        let concrete = await node.evaluate(in: .init()).concrete
+        #expect(BoundingBox3D(concrete.bounds) ≈ BoundingBox3D(minimum: [-2.5, -2.5, -2.5], maximum: [20, 4, 4]))
+    }
 }
