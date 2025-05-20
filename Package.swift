f2489a03d4d0059151636635f472ef3fe440e3da
Tomas Franzén <tomas@tomasf.se>
2025-05-20 17:41:17 +0200

    Add Text support, so far without lookup support

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/tomasf/manifold-swift.git", branch: "dev"), //.upToNextMinor(from: "0.2.0")
         .package(url: "https://github.com/tomasf/ThreeMF.git", branch: "main"),
+        .package(url: "https://github.com/tomasf/freetype-spm.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
@@ -19,6 +20,7 @@ let package = Package(
             name: "Cadova",
             dependencies: [
                 .byName(name: "CadovaCPP"),
+                .product(name: "freetype", package: "freetype-spm"),
                 .product(name: "Manifold", package: "manifold-swift"),
                 .product(name: "ThreeMF", package: "ThreeMF"),
                 .product(name: "Collections", package: "swift-collections"),
