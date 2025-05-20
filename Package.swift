28fada729ff4962edab1c04562fa71e3ae2a681f
Tomas Franzén <tomas@tomasf.se>
2025-05-20 20:29:16 +0200

    Add font lookup

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         .package(url: "https://github.com/tomasf/manifold-swift.git", branch: "dev"), //.upToNextMinor(from: "0.2.0")
         .package(url: "https://github.com/tomasf/ThreeMF.git", branch: "main"),
         .package(url: "https://github.com/tomasf/freetype-spm.git", branch: "main"),
+        .package(url: "https://github.com/tomasf/FindFont.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
@@ -21,6 +22,7 @@ let package = Package(
             dependencies: [
                 .byName(name: "CadovaCPP"),
                 .product(name: "freetype", package: "freetype-spm"),
+                .product(name: "FindFont", package: "FindFont"),
                 .product(name: "Manifold", package: "manifold-swift"),
                 .product(name: "ThreeMF", package: "ThreeMF"),
                 .product(name: "Collections", package: "swift-collections"),
