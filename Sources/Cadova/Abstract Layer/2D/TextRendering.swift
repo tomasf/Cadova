3fb29d914baf3c2340855e317fd5ebf21e4ca5f6
Tomas Franzén <tomas@tomasf.se>
2025-05-21 11:20:28 +0200

    Use updated FindFont

--- Sources/Cadova/Abstract Layer/2D/TextRendering.swift
+++ Sources/Cadova/Abstract Layer/2D/TextRendering.swift
@@ -15,13 +15,18 @@ extension TextAttributes {
         }
 
         let fontData: Data
+        let effectiveFace: (family: String, style: String?)
+
         if let fontFile {
             fontData = try! Data(contentsOf: fontFile)
+            effectiveFace = (family, fontFace?.style)
+
         } else {
-            guard let data = try FontRepository.dataForFont(family: family, style: fontFace?.style) else {
+            guard let match = try FontRepository.matchForFont(family: family, style: fontFace?.style) else {
                 throw TextError.fontNotFound(family: family, style: fontFace?.style)
             }
-            fontData = data
+            fontData = match.data
+            effectiveFace = (match.familyName, match.style)
         }
 
         var library: FT_Library?
@@ -29,14 +34,7 @@ extension TextAttributes {
             throw TextError.freeTypeInitializationFailure
         }
 
-        var face: FT_Face?
-        if let style = fontFace?.style {
-            face = try library.faceMatching(family: family, style: style, from: fontData)
-        } else {
-            face = try library.primaryFace(matchingFamily: family, from: fontData)
-        }
-
-        guard let face else {
+        guard let face = try library.faceMatching(family: effectiveFace.family, style: effectiveFace.style, from: fontData) else {
             throw TextError.fontLoadingFailed
         }
 
@@ -76,7 +74,7 @@ extension TextAttributes {
 
 fileprivate extension FT_Library {
     // Caller is responsible for calling FT_Done_Face on a returned face
-    func faceMatching(family targetFamily: String?, style targetStyle: String?, from fontData: Data) throws -> FT_Face? {
+    func faceMatching(family targetFamily: String, style targetStyle: String?, from fontData: Data) throws -> FT_Face? {
         var face: FT_Face?
         let loadResult = fontData.withUnsafeBytes { buffer in
             FT_New_Memory_Face(self, buffer.baseAddress, buffer.count, -1, &face)
@@ -85,7 +83,7 @@ fileprivate extension FT_Library {
             throw TextError.fontLoadingFailed
         }
 
-        let faceCount = dummyFace.pointee.num_faces ?? 0
+        let faceCount = dummyFace.pointee.num_faces
         FT_Done_Face(dummyFace)
 
         for i in 0..<faceCount {
@@ -99,69 +97,12 @@ fileprivate extension FT_Library {
             let familyName = String(cString: face.pointee.family_name)
             let faceName = String(cString: face.pointee.style_name)
 
-            if (familyName == targetFamily || targetFamily == nil), (faceName == targetStyle || targetStyle == nil) {
+            if (familyName == targetFamily), (faceName == targetStyle || targetStyle == nil) {
                 return face
             }
 
             FT_Done_Face(face)
         }
-        return nil
-    }
-
-    // Matches a font family name without a style
-    // Caller is responsible for calling FT_Done_Face on a returned face
-    func primaryFace(matchingFamily targetFamily: String, from fontData: Data) throws -> FT_Face? {
-        var face: FT_Face?
-        let loadResult = fontData.withUnsafeBytes { buffer in
-            FT_New_Memory_Face(self, buffer.baseAddress, buffer.count, -1, &face)
-        }
-        guard loadResult == 0, let dummyFace = face else {
-            throw TextError.fontLoadingFailed
-        }
-
-        let faceCount = dummyFace.pointee.num_faces ?? 0
-        FT_Done_Face(dummyFace)
-
-        let allFaces = try (0..<faceCount).compactMap { faceIndex in
-            var face: FT_Face?
-            let loadResult = fontData.withUnsafeBytes { buffer in
-                FT_New_Memory_Face(self, buffer.baseAddress, buffer.count, faceIndex, &face)
-            }
-            guard loadResult == 0, let face else {
-                throw TextError.fontLoadingFailed
-            }
-
-            let familyName = String(cString: face.pointee.family_name)
-            if familyName == targetFamily {
-                return face
-            } else {
-                FT_Done_Face(face)
-                return nil
-            }
-        }
-
-        defer {
-            allFaces.forEach { FT_Done_Face($0) }
-        }
-
-        // First, check if there is a style called "Regular"
-        if let regularMatch = allFaces.first(where: { String(cString: $0.pointee.style_name) == "Regular" }) {
-            FT_Reference_Face(regularMatch)
-            return regularMatch
-        }
-
-        // If not, try matching one without bold and italic flags
-        if let plainMatch = allFaces.first(where: { $0.pointee.style_flags & FT_Long(FT_STYLE_FLAG_ITALIC | FT_STYLE_FLAG_BOLD) == 0 }) {
-            FT_Reference_Face(plainMatch)
-            return plainMatch
-        }
-
-        // Otherwise, fuck it
-        if let first = allFaces.first {
-            FT_Reference_Face(first)
-            return first
-        }
-
         return nil
     }
 }
