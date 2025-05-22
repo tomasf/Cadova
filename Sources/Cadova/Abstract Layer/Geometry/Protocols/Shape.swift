7c34c3b021794b75909ba89bde305756f0ce2814
Tomas Franzén <tomas@tomasf.se>
2025-05-22 09:05:13 +0200

    Improve shape

--- Sources/Cadova/Abstract Layer/Geometry/Protocols/Shape.swift
+++ Sources/Cadova/Abstract Layer/Geometry/Protocols/Shape.swift
@@ -1,5 +1,25 @@
 import Foundation
 
+public protocol Shape: Geometry {
+    @GeometryBuilder<D> var body: any Geometry<D> { get }
+}
+
+public extension Shape {
+    func build(in environment: EnvironmentValues, context: EvaluationContext) async throws -> D.BuildResult {
+        try await environment.whileCurrent {
+            try await body.build(in: environment, context: context)
+        }
+    }
+
+    func inCurrentEnvironment(@GeometryBuilder<D> _ contents: @Sendable @escaping () -> D.Geometry) -> D.Geometry {
+        readEnvironment { environment in
+            environment.whileCurrent {
+                contents()
+            }
+        }
+    }
+}
+
 /// A protocol defining the requirements for custom 2D shapes.
 ///
 /// Conform to `Shape2D` to create custom types that represent 2D geometries. A conforming type must provide a `body` property, which defines the shape's geometry using geometry primitives and operations. The `Shape2D` protocol itself conforms to `Geometry2D`, ensuring that custom shapes can be used anywhere standard Cadova 2D geometries are used.
@@ -7,32 +27,18 @@ import Foundation
 /// Example:
 /// ```
 /// struct CustomShape: Shape2D {
-///     var body: some Geometry2D {
-///         // Define the shape
+///     let size: Double
+///
+///     var body: any Geometry2D {
+///         Rectangle(x: size * 2, y: size)
 ///     }
 /// }
 /// ```
-public protocol Shape2D: Geometry2D {
+public protocol Shape2D: Shape where D == D2 {
     /// The geometry content of this shape.
     ///
     /// Implement this property to define the shape's structure using Cadova's geometry primitives and operations.
     @GeometryBuilder2D var body: any Geometry2D { get }
-}
-
-public extension Shape2D {
-    func build(in environment: EnvironmentValues, context: EvaluationContext) async throws -> D.BuildResult {
-        try await environment.whileCurrent {
-            try await body.build(in: environment, context: context)
-        }
-    }
-
-    func inCurrentEnvironment(@GeometryBuilder2D _ contents: @Sendable @escaping () -> any Geometry2D) -> any Geometry2D {
-        readEnvironment { environment in
-            environment.whileCurrent {
-                contents()
-            }
-        }
-    }
 }
 
 /// A protocol defining the requirements for custom 3D shapes.
@@ -42,30 +48,16 @@ public extension Shape2D {
 /// Example:
 /// ```
 /// struct CustomShape: Shape3D {
-///     var body: some Geometry3D {
-///         // Define the shape
+///     let size: Double
+///
+///     var body: any Geometry3D {
+///         Box(x: size, y: size, z: size * 3)
 ///     }
 /// }
 /// ```
-public protocol Shape3D: Geometry3D {
+public protocol Shape3D: Shape where D == D3 {
     /// The geometry content of this shape.
     ///
     /// Implement this property to define the shape's structure using Cadova's geometry primitives and operations.
     @GeometryBuilder3D var body: any Geometry3D { get }
-}
-
-public extension Shape3D {
-    func build(in environment: EnvironmentValues, context: EvaluationContext) async throws -> D.BuildResult {
-        try await environment.whileCurrent {
-            try await body.build(in: environment, context: context)
-        }
-    }
-
-    func inCurrentEnvironment(@GeometryBuilder3D _ contents: @Sendable @escaping () -> any Geometry3D) -> any Geometry3D {
-        readEnvironment { environment in
-            environment.whileCurrent {
-                contents()
-            }
-        }
-    }
 }
