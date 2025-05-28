63e1bf5fbd1e9857ac244ee5bd12e27c7ff3dbb8
Tomas Franzén <tomas@tomasf.se>
2025-05-28 23:02:47 +0200

    Remove CachedBoxedGeometry and simplify EvaluationContext

--- Sources/Cadova/Node Layer/Context/ResultElementCache.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-actor ResultElementCache {
-    internal var entries: [OpaqueKey: ResultElements] = [:]
-
-    func setResultElements(_ resultElements: ResultElements?, for key: OpaqueKey) {
-        entries[key] = resultElements
-    }
-}
