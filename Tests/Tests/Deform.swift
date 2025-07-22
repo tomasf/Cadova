a3941168034d9b99ec560ed47599cf41115d1465
Tomas Franzén <tomas@tomasf.se>
2025-07-22 16:26:21 +0200

    Update tests to match simplified deformation
    Simplification changes the measurements slightly

--- Tests/Tests/Deform.swift
+++ Tests/Tests/Deform.swift
@@ -10,21 +10,21 @@ struct DeformTests {
             }, with: .x)
 
         let m = try await deformation.measurements
-        #expect(m.area ≈ 500)
-        #expect(m.boundingBox ≈ .init(minimum: [0, -20.8497], maximum: [50, 35]))
+        #expect(m.area ≈ 499.992)
+        #expect(m.boundingBox ≈ .init(minimum: [0, -20.8497], maximum: [50, 34.999]))
     }
 
     @Test func basic2Din3D() async throws {
         let deformation = Box(x: 100, y: 3, z: 20)
-            .deformed(using: BezierPath2D {
+            .deforming(.y, along: .x, using: BezierPath2D {
                 curve(controlX: 50, controlY: 50, endX: 100, endY: 0)
-            }, with: .x)
+            })
             .withSegmentation(count: 100)
 
         let m = try await deformation.measurements
 
-        #expect(m.volume ≈ 6000)
+        #expect(m.volume ≈ 6000.044)
         #expect(m.surfaceArea ≈ 5311.032)
-        #expect(m.boundingBox ≈ .init(minimum: .zero, maximum: [100, 28, 20]))
+        #expect(m.boundingBox ≈ .init(minimum: .zero, maximum: [100, 27.998, 20]))
     }
 }
