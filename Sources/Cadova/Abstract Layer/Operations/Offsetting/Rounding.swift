9696f9b12766faac2476a9850ed936fca52a0377
Tomas Franzén <tomas@tomasf.se>
2025-07-22 16:59:35 +0200

    Deprecate rounding with mask

--- Sources/Cadova/Abstract Layer/Operations/Offsetting/Rounding.swift
+++ Sources/Cadova/Abstract Layer/Operations/Offsetting/Rounding.swift
@@ -38,44 +38,4 @@ public extension Geometry2D {
     func rounded(radius: Double) -> any Geometry2D {
         rounded(insideRadius: radius, outsideRadius: radius)
     }
-
-    /// Applies uniform rounding to the geometry's corners, limited to areas covered by a mask.
-    ///
-    /// This is a convenience method that applies the same rounding radius to both inside and outside edges,
-    /// but restricts the effect to the area defined by the given mask.
-    /// Equivalent to calling `rounded(insideRadius:radius, outsideRadius:radius, in: mask)`.
-    ///
-    /// - Parameters:
-    ///   - radius: The radius to apply to both inside and outside edges.
-    ///   - mask: A closure that defines the mask geometry, limiting where the rounding is applied.
-    /// - Returns: A new geometry object with rounded corners, limited to the area covered by the mask.
-    func rounded(radius: Double, @GeometryBuilder2D in mask: @Sendable @escaping () -> any Geometry2D) -> any Geometry2D {
-        rounded(insideRadius: radius, outsideRadius: radius, in: mask)
-    }
-
-    /// Applies rounding to the geometry's corners with separate inside and outside radii, limited to areas covered by
-    /// a mask.
-    ///
-    /// This method combines the base geometry with a specified mask, rounding the geometry's corners only within the
-    /// mask's boundaries. The `insideRadius` and `outsideRadius` parameters determine the extent of rounding on each
-    /// side, respectively.
-    ///
-    /// - Parameters:
-    ///   - insideRadius: The radius of rounding applied to the inside edges of the geometry. Optional.
-    ///   - outsideRadius: The radius of rounding applied to the outside edges of the geometry. Optional.
-    ///   - mask: A closure that defines the mask geometry, limiting where the rounding is applied.
-    /// - Returns: A new geometry object with rounded corners, limited to the area covered by the mask.
-    ///
-    func rounded(
-        insideRadius: Double? = nil,
-        outsideRadius: Double? = nil,
-        @GeometryBuilder2D in mask: @Sendable @escaping () -> any Geometry2D
-    ) -> any Geometry2D {
-        let maskShape = Deferred(mask)
-        return subtracting(maskShape)
-            .adding {
-                self.rounded(insideRadius: insideRadius, outsideRadius: outsideRadius)
-                    .intersecting(maskShape)
-            }
-    }
 }
