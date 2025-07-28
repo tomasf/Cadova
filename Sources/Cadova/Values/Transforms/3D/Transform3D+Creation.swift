83f78aa79ad18bcc76b64b1d33350452934737ae
Tomas Franzén <tomas@tomasf.se>
2025-07-28 14:59:57 +0200

    Add RepeatedAlongPath

--- Sources/Cadova/Values/Transforms/3D/Transform3D+Creation.swift
+++ Sources/Cadova/Values/Transforms/3D/Transform3D+Creation.swift
@@ -93,7 +93,7 @@ public extension Transform3D {
 
     static func rotation(from: Direction3D, to: Direction3D) -> Transform3D {
         let axis = Direction3D(from.unitVector × to.unitVector)
-        let angle: Angle = acos(from.unitVector ⋅ to.unitVector)
+        let angle: Angle = acos((from.unitVector ⋅ to.unitVector).clamped(to: -1.0...1.0))
         return .rotation(angle: angle, around: axis)
     }
 
