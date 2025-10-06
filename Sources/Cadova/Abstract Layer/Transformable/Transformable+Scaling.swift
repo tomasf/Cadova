c263eafb3216d170f3af30bb3a270aae578355f2
Tomas Franzén <tomas@tomasf.se>
2025-10-06 14:35:13 +0200

    Move transformation from Geometry into new Transformable protocol

---
+++ Sources/Cadova/Abstract Layer/Transformable/Transformable+Scaling.swift
@@ -0,0 +1,68 @@
+import Foundation
+
+public extension Transformable {
+    /// Scale object uniformly or non-uniformly.
+    ///
+    /// This method allows scaling the object by a specified vector, where each component of the vector represents
+    /// the scaling factor along the corresponding axis.
+    ///
+    /// - Parameters:
+    ///   - scale: A `Vector2D`/`Vector3D` representing the scaling factors along the axes.
+    /// - Returns: A scaled object.
+    func scaled(_ scale: D.Vector) -> Transformed {
+        transformed(.scaling(scale))
+    }
+
+    /// Scale object uniformly.
+    ///
+    /// This method scales the object uniformly across all axes using a single factor.
+    ///
+    /// - Parameters:
+    ///   - factor: The uniform scaling factor.
+    /// - Returns: A uniformly scaled object.
+    func scaled(_ factor: Double) -> Transformed {
+        scaled(D.Vector(factor))
+    }
+
+    /// Flips the object along the specified axes.
+    ///
+    /// This method reflects the object along the given axes by negating the coordinate values along them.
+    /// It can be used to create mirrored versions of an object or invert the object across one or more axes.
+    ///
+    /// - Parameter axes: The axes across which to flip the object.
+    /// - Returns: A new object that is mirrored across the specified axes.
+    func flipped(along axes: D.Axes) -> Transformed {
+        scaled(D.Vector(1).with(axes, as: -1))
+    }
+}
+
+public extension Transformable<D2> {
+    /// Scale object non-uniformly.
+    ///
+    /// This method allows non-uniform scaling of the object by specifying individual scaling factors for the x and
+    /// y axes.
+    ///
+    /// - Parameters:
+    ///   - x: The scaling factor along the x-axis.
+    ///   - y: The scaling factor along the y-axis.
+    /// - Returns: A non-uniformly scaled object.
+    func scaled(x: Double = 1, y: Double = 1) -> Transformed {
+        scaled(Vector2D(x, y))
+    }
+}
+
+public extension Transformable<D3> {
+    /// Scale object non-uniformly.
+    ///
+    /// This method allows non-uniform scaling of the object by specifying individual scaling factors for the x, y,
+    /// and z axes.
+    ///
+    /// - Parameters:
+    ///   - x: The scaling factor along the x-axis.
+    ///   - y: The scaling factor along the y-axis.
+    ///   - z: The scaling factor along the z-axis.
+    /// - Returns: A non-uniformly scaled object.
+    func scaled(x: Double = 1, y: Double = 1, z: Double = 1) -> Transformed {
+        scaled(Vector3D(x, y, z))
+    }
+}
