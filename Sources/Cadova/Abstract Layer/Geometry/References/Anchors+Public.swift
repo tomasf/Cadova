1faf5849bfe0c179e99161a811abecf1ec246463
Tomas Franzén <tomas@tomasf.se>
2026-04-28 11:58:54 +0200

    Fix DocC for 2D definingAnchor parameters

--- Sources/Cadova/Abstract Layer/Geometry/References/Anchors+Public.swift
+++ Sources/Cadova/Abstract Layer/Geometry/References/Anchors+Public.swift
@@ -138,14 +138,13 @@ public extension Geometry2D {
     /// Defines an anchor point.
     ///
     /// Use this method to mark a specific coordinate system as an anchor. The anchor captures the current
-    /// transformation state, optionally applying an additional alignment, offset, direction, and rotation around the
-    /// direction vector. The resulting world transform is recorded for later use by `anchored(to:)`.
+    /// transformation state, optionally applying an additional alignment, offset, and rotation. The resulting world
+    /// transform is recorded for later use by `anchored(to:)`.
     ///
     /// The applied transform is constructed by:
     /// 1) applying the specified alignment (if any),
     /// 2) translating by `offset`,
-    /// 3) rotating around Z by `rotation`,
-    /// 4) rotating from `.up` to `direction`.
+    /// 3) rotating by `rotation`.
     ///
     /// - Important:
     ///   - The same `Anchor` can be defined multiple times. Each call records another world transform. When you later
@@ -158,10 +157,8 @@ public extension Geometry2D {
     ///   - anchor: The `Anchor` to define on this geometry.
     ///   - alignment: One or more alignment options specifying where on the geometry the anchor should be located. If
     ///     no alignment is specified, the origin is used.
-    ///   - offset: An optional `Vector3D` used to offset the anchor.
-    ///   - direction: An optional direction vector relative to the current orientation, applied after alignment and
-    ///     offset. This direction becomes the positive Z of this anchor.
-    ///   - rotation: An optional rotation around the direction vector.
+    ///   - offset: An optional `Vector2D` used to offset the anchor.
+    ///   - rotation: An optional rotation applied to the anchor.
     /// - Returns: The geometry with a defined anchor.
     ///
     func definingAnchor(
